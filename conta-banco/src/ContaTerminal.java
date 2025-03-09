dd0453d7bbfbd4ea96a8d54f41da7d74c85ee7b5
Rodrigo Souza <ro6rigo@gmail.com>
2025-03-09 18:05:04 -0300

    feat: adicionado leitura e escrita de informacoes do usuario

--- conta-banco/src/ContaTerminal.java
+++ conta-banco/src/ContaTerminal.java
@@ -1,5 +1,22 @@
+import java.util.Scanner;
 public class ContaTerminal {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Por favor, digite o número da Agência !");
+        String agencia = scanner.next();
+        
+        System.out.println("Por favor, digite o número da Conta !");
+        int numero = scanner.nextInt();
+        
+        System.out.println("Por favor, digite o nome do Cliente !");
+        String nomeCliente = scanner.next();
+
+        System.out.println("Por favor, digite o saldo da Conta !");
+        String saldoTexto = scanner.next();
+        double saldo = Double.parseDouble(saldoTexto);
+        scanner.close();
+
+        System.out.println( "Olá "+nomeCliente+", obrigado por criar uma conta em nosso banco, sua agência é "+agencia+", conta "+numero+" e seu saldo "+saldo+" já está disponível para saque");
     }
 }
